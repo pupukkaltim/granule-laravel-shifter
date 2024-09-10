ff674539bfde6b50045c9da00baf80c19f9a7aa9
sallieeky <sallieeky@gmail.com>
2024-09-10 19:34:16 +0800

    🔀 chore(ShiftToLaravel11.php): uncomment composer commands to upgrade dependencies and remove unnecessary comments 🔀 chore(ShiftToLaravel11.php): update composer.json with new versions of dependencies to shift to Laravel 11

--- src/Console/ShiftToLaravel11.php
+++ src/Console/ShiftToLaravel11.php
@@ -77,43 +77,57 @@ trait ShiftToLaravel11
         //     "spatie/laravel-ignition": "^2.0"
         // },
 
-        // $this->replaceContent(base_path('composer.json'), [
-        //     '"granule/starter-kit": "^5.1"' => '"granule/starter-kit": "dev-v6-dev"',
-        //     '"inertiajs/inertia-laravel": "^0.6.8"' => '"inertiajs/inertia-laravel": "^1.3.0"',
-        //     '"laravel/framework": "^10.10"' => '"laravel/framework": "^11.9"',
-        //     '"laravel/reverb": "@beta"' => '"laravel/reverb": "^1.3"',
-        //     '"laravel/sanctum": "^3.2"' => '"laravel/sanctum": "^4.0"',
-        //     '"laravel/tinker": "^2.8"' => '"laravel/tinker": "^2.9"',
+        $this->replaceContent(base_path('composer.json'), [
+            '"granule/starter-kit": "^5.1"' => '"granule/starter-kit": "dev-v6-dev"',
+            '"inertiajs/inertia-laravel": "^0.6.8"' => '"inertiajs/inertia-laravel": "^1.3.0"',
+            '"laravel/framework": "^10.10"' => '"laravel/framework": "^11.9"',
+            '"laravel/reverb": "@beta"' => '"laravel/reverb": "^1.3"',
+            '"laravel/sanctum": "^3.2"' => '"laravel/sanctum": "^4.0"',
+            '"laravel/tinker": "^2.8"' => '"laravel/tinker": "^2.9"',
 
-        //     '"fakerphp/faker": "^1.9.1"' => '"fakerphp/faker": "^1.23"',
-        //     '"laravel/breeze": "v1.29"' => '"laravel/breeze": "v2.1"',
-        //     '"laravel/pint": "^1.0"' => '"laravel/pint": "^1.13"',
-        //     '"laravel/sail": "^1.18"' => '"laravel/sail": "^1.26"',
-        //     '"mockery/mockery": "^1.4.4"' => '"mockery/mockery": "^1.6"',
-        //     '"nunomaduro/collision": "^7.0"' => '"nunomaduro/collision": "^8.1"',
-        //     '"pestphp/pest": "^2.0"' => '"pestphp/pest": "^2.0"',
+            '"fakerphp/faker": "^1.9.1"' => '"fakerphp/faker": "^1.23"',
+            '"laravel/breeze": "v1.29"' => '"laravel/breeze": "v2.1"',
+            '"laravel/pint": "^1.0"' => '"laravel/pint": "^1.13"',
+            '"laravel/sail": "^1.18"' => '"laravel/sail": "^1.26"',
+            '"mockery/mockery": "^1.4.4"' => '"mockery/mockery": "^1.6"',
+            '"nunomaduro/collision": "^7.0"' => '"nunomaduro/collision": "^8.1"',
+            '"pestphp/pest": "^2.0"' => '"pestphp/pest": "^2.0"',
+        ]);
+
+        // upgrade composer.json
+        $this->runCommands([
+            'composer require granule/starter-kit:dev-v6-dev 
+                inertiajs/inertia-laravel:^1.3.0 
+                laravel/framework:^11.9 
+                laravel/reverb:^1.3 
+                laravel/sanctum:^4.0 
+                laravel/tinker:^2.9 
+                -W --no-scripts --quiet --no-interaction',
+
+            'composer require fakerphp/faker:^1.23
+                laravel/breeze:v2.1 
+                laravel/pint:^1.13 
+                laravel/sail:^1.26 
+                mockery/mockery:^1.6 
+                nunomaduro/collision:^8.1 
+                -W --dev --no-scripts --quiet --no-interaction',
+        ]);
+
+        // upgrade composer.json
+        // $this->runCommands([
+        //     'composer require granule/starter-kit:dev-v6-dev --no-install',
+        //     'composer require inertiajs/inertia-laravel:^1.3.0 --no-install',
+        //     'composer require laravel/framework:^11.9 --no-install',
+        //     'composer require laravel/reverb:^1.3 --no-install',
+        //     'composer require laravel/sanctum:^4.0 --no-install',
+        //     'composer require laravel/tinker:^2.9 --no-install',
+
+        //     'composer require fakerphp/faker:^1.23 --dev --no-install',
+        //     'composer require laravel/breeze:v2.1 --dev --no-install',
+        //     'composer require laravel/pint:^1.13 --dev --no-install',
+        //     'composer require laravel/sail:^1.26 --dev --no-install',
+        //     'composer require mockery/mockery:^1.6 --dev --no-install',
+        //     'composer require nunomaduro/collision:^8.1 --dev --no-install',
         // ]);
-
-        // upgrade composer.json
-        $this->runCommands([
-            'composer require granule/starter-kit:dev-v6-dev --no-install',
-            'composer require inertiajs/inertia-laravel:^1.3.0 --no-install',
-            'composer require laravel/framework:^11.9 --no-install',
-            'composer require laravel/reverb:^1.3 --no-install',
-            'composer require laravel/sanctum:^4.0 --no-install',
-            'composer require laravel/tinker:^2.9 --no-install',
-
-            'composer require fakerphp/faker:^1.23 --dev --no-install',
-            'composer require laravel/breeze:v2.1 --dev --no-install',
-            'composer require laravel/pint:^1.13 --dev --no-install',
-            'composer require laravel/sail:^1.26 --dev --no-install',
-            'composer require mockery/mockery:^1.6 --dev --no-install',
-            'composer require nunomaduro/collision:^8.1 --dev --no-install',
-        ]);
-
-        // upgrade composer.json
-        $this->runCommands([
-            'composer update -W',
-        ]);
     }
 }
