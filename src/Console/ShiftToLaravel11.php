6a3d4675cb1b0bc71a7dee52831c801a65a16baa
sallieeky <sallieeky@gmail.com>
2024-09-10 21:03:57 +0800

    🔀 chore(ShiftToLaravel11.php): update package versions in composer.json to shift to Laravel 11
    The package versions in the composer.json file have been updated to shift to Laravel 11. The following changes were made:
    
    - Updated "granule/starter-kit" package to "dev-v6-dev"
    - Updated "inertiajs/inertia-laravel" package to "^1.3.0"
    - Updated "laravel/framework" package to "^11.9"
    - Updated "laravel/reverb" package to "^1.3"
    - Updated "laravel/sanctum" package to "^4.0"
    - Updated "laravel/tinker" package to "^2.9"
    - Updated "fakerphp/faker" package to "^1.23"
    - Updated "laravel/breeze" package to "v2.1"
    - Updated "laravel/pint" package to "^1.13"
    - Updated "laravel/sail" package to "^1.26"
    - Updated "mockery/mockery" package to "^1.6"
    - Updated "nunomaduro/collision" package to "^8.1"
    
    Additionally, the composer update command was run to apply the changes.

--- src/Console/ShiftToLaravel11.php
+++ src/Console/ShiftToLaravel11.php
@@ -21,96 +21,11 @@ trait ShiftToLaravel11
             '"php": "^8.1"' => '"php": "^8.2"',
         ]);
 
-        // from
-        // "require": {
-        //     "php": "^8.1",
-        //     "doctrine/dbal": "*",
-        //     "granule/starter-kit": "^5.1",
-        //     "guzzlehttp/guzzle": "^7.2",
-        //     "inertiajs/inertia-laravel": "^0.6.8",
-        //     "laravel/framework": "^10.10",
-        //     "laravel/reverb": "@beta",
-        //     "laravel/sanctum": "^3.2",
-        //     "laravel/tinker": "^2.8",
-        //     "pusher/pusher-php-server": "^7.2",
-        //     "spatie/laravel-permission": "^6.1",
-        //     "staudenmeir/laravel-migration-views": "^1.7",
-        //     "tightenco/ziggy": "^2.0"
-        // },
-        // "require-dev": {
-        //     "fakerphp/faker": "^1.9.1",
-        //     "laravel/breeze": "v1.29",
-        //     "laravel/pint": "^1.0",
-        //     "laravel/sail": "^1.18",
-        //     "mockery/mockery": "^1.4.4",
-        //     "nunomaduro/collision": "^7.0",
-        //     "pestphp/pest": "^2.0",
-        //     "pestphp/pest-plugin-laravel": "^2.0",
-        //     "spatie/laravel-ignition": "^2.0"
-        // },
-
-        // to
-        // "require": {
-        //     "php": "^8.2",
-        //     "doctrine/dbal": "*",
-        //     "granule/starter-kit": "dev-v6-dev",
-        //     "guzzlehttp/guzzle": "^7.2",
-        //     "inertiajs/inertia-laravel": "^1.3.0",
-        //     "laravel/framework": "^11.9",
-        //     "laravel/reverb": "^1.3",
-        //     "laravel/sanctum": "^4.0",
-        //     "laravel/tinker": "^2.9",
-        //     "pusher/pusher-php-server": "^7.2",
-        //     "spatie/laravel-permission": "^6.1",
-        //     "staudenmeir/laravel-migration-views": "^1.7",
-        //     "tightenco/ziggy": "^2.0"
-        // },
-        // "require-dev": {
-        //     "fakerphp/faker": "^1.23",
-        //     "laravel/breeze": "v2.1",
-        //     "laravel/pint": "^1.13",
-        //     "laravel/sail": "^1.26",
-        //     "mockery/mockery": "^1.6",
-        //     "nunomaduro/collision": "^8.1",
-        //     "pestphp/pest": "^2.0",
-        //     "pestphp/pest-plugin-laravel": "^2.0",
-        //     "spatie/laravel-ignition": "^2.0"
-        // },
-
-        // upgrade composer.json
+        // change package versions
         $this->runCommands([
-            'composer require granule/starter-kit:dev-v6-dev 
-                inertiajs/inertia-laravel:^1.3.0 
-                laravel/framework:^11.9 
-                laravel/reverb:^1.3 
-                laravel/sanctum:^4.0 
-                laravel/tinker:^2.9 
-                -W --no-scripts --quiet --no-interaction',
-
-            'composer require fakerphp/faker:^1.23
-                laravel/breeze:v2.1 
-                laravel/pint:^1.13 
-                laravel/sail:^1.26 
-                mockery/mockery:^1.6 
-                nunomaduro/collision:^8.1 
-                -W --dev --no-scripts --quiet --no-interaction',
+            'composer require inertiajs/inertia-laravel:^1.3.0 laravel/framework:^11.22 laravel/reverb:^1.3 laravel/sanctum:^4.0 laravel/tinker:^2.9 --no-update --quiet',
+            'composer require fakerphp/faker:^1.23 laravel/breeze:v2.1 laravel/pint:^1.13 laravel/sail:^1.26 mockery/mockery:^1.6 nunomaduro/collision:^8.1 --dev --no-update --quiet',
+            'composer update -W --no-scripts --no-interaction'
         ]);
-
-        // upgrade composer.json
-        // $this->runCommands([
-        //     'composer require granule/starter-kit:dev-v6-dev --no-install',
-        //     'composer require inertiajs/inertia-laravel:^1.3.0 --no-install',
-        //     'composer require laravel/framework:^11.9 --no-install',
-        //     'composer require laravel/reverb:^1.3 --no-install',
-        //     'composer require laravel/sanctum:^4.0 --no-install',
-        //     'composer require laravel/tinker:^2.9 --no-install',
-
-        //     'composer require fakerphp/faker:^1.23 --dev --no-install',
-        //     'composer require laravel/breeze:v2.1 --dev --no-install',
-        //     'composer require laravel/pint:^1.13 --dev --no-install',
-        //     'composer require laravel/sail:^1.26 --dev --no-install',
-        //     'composer require mockery/mockery:^1.6 --dev --no-install',
-        //     'composer require nunomaduro/collision:^8.1 --dev --no-install',
-        // ]);
     }
 }
