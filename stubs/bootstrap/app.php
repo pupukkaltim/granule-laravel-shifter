0c6475a42f253eba3207675edf1fd8c7b2995453
sallieeky <sallieeky@gmail.com>
2024-10-01 09:27:10 +0800

    🚀 feat(app.php): add support for API routes by including api.php file in the routes configuration

--- stubs/bootstrap/app.php
+++ stubs/bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: [__DIR__.'/../routes/web.php', __DIR__.'/../routes/starter.php'],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        api: [__DIR__.'/../routes/api.php'],
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append([
